6f8d51e1198650afe70893106e36e2823d8f97d3
KittoNoKatsu <https://github.com/KittoNoKatsu>
2023-11-05 19:10:08 +0400

    Developed functionality for transferring data from csv to postgresql

--- etl/models.py
+++ etl/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class CsvData(models.Model):
+    date = models.DateField(auto_now= False)
+    cases = models.IntegerField(default=True)
+    deaths = models.IntegerField(default=True)
+    cases_per_100000 = models.FloatField(default=True, null=True, blank=True)

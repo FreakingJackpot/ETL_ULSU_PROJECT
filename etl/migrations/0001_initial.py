99d8d793f0e006aeaa2814ffcb6d9fc84ed80439
KittoNoKatsu <https://github.com/KittoNoKatsu>
2023-11-05 21:32:17 +0400

    Rename migration

--- etl/migrations/0001_initial.py
+++ etl/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Data_about_infected',
+            name='CsvData',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('date', models.DateField()),

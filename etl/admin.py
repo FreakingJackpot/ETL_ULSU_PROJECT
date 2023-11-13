75f0dc6ff957047b54d902e32b6fdf28a87f22f6
Sergey Sergeev <sss30112001@yandex.ru>
2023-11-13 17:29:22 +0400

    add models to admin

--- etl/admin.py
+++ etl/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from etl.models import StopCoronaData, CsvData
+from etl.models import StopCoronaData, CsvData,GogovRegionData,GogovGlobalData,ExternalDatabaseStatistic,ExternalDatabaseVaccination
 
 # Register your models here.
 admin.site.register(CsvData)
 admin.site.register(StopCoronaData)
+admin.site.register(GogovGlobalData)
+admin.site.register(GogovRegionData)

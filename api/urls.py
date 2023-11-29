c13b8ab41a208ac7d22948c92ccac86ded574e92
nastyaqw <nastya22bir@gmail.com>
2023-11-29 12:13:18 +0400

    add login API

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterAPI, LoginAPI, UserAPI
-
-urlpatterns = [
-    path('api/register/', RegisterAPI.as_view(), name='register'),
-    path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/user/', UserAPI.as_view(), name='user'),
-]

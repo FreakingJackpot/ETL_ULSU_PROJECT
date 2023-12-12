39eb64aa5352070d07836a4c664fcd63309b22e4
Sergey Sergeev <sss30112001@yandex.ru>
2023-12-13 03:09:43 +0400

    after merge fix, add requirements.txt

--- app/apps/api/v1/urls.py
+++ app/apps/api/v1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import LoginAPI
 
 urlpatterns = [
-    path('v1/login/', LoginAPI.as_view(), name='login'),
+    path('login/', LoginAPI.as_view(), name='login'),
 ]
--- app/apps/api/v1/views.py
+++ app/apps/api/v1/views.py
@@ -1,30 +1,24 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics
 from knox.models import AuthToken
-from knox.views import LogoutView, AllLogoutView
+from knox.auth import TokenAuthentication
 from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
+from rest_framework.response import Response
+
 from .serializers import UserSerializer
+
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = UserSerializer
-    authentication = [knox.auth.TokenAuthentication]
+    authentication = [TokenAuthentication]
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-        AuthToken.objects.filter(user=user).delete() #удаление старых токенов
+        AuthToken.objects.filter(user=user).delete()  # удаление старых токенов
         _, token = AuthToken.objects.create(user)
-        user_logged_in.send(sender=user.__class__, request=request, user=user) #отправка о входе
+        user_logged_in.send(sender=user.__class__, request=request, user=user)  # отправка о входе
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": token
         })
-
-
-
-
-    
-
-
-

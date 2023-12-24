9e427b7c5946e5453f68b8450250f2bfe4537627
Sergey Sergeev <sss30112001@yandex.ru>
2023-12-24 16:22:20 +0400

    add datasets info api and dataset api, swagger, xml and csv renderers

--- app/apps/api/v1/views.py
+++ app/apps/api/v1/views.py
@@ -1,10 +1,16 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
+from rest_framework.settings import api_settings
+from rest_framework_csv import renderers as r
 from knox.models import AuthToken
 from knox.auth import TokenAuthentication
 from django.contrib.auth.signals import user_logged_in
-from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, DatasetInfoSerializer, DatasetRequestSerializer
+from apps.api.models import DatasetInfo
+from apps.etl.models import RegionTransformedData
 
 
 class LoginAPI(generics.GenericAPIView):
@@ -22,3 +28,56 @@ class LoginAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": token
         })
+
+
+class DatasetsInfo(generics.ListAPIView):
+    queryset = DatasetInfo.objects.all()
+    serializer_class = DatasetInfoSerializer
+
+
+class Dataset(generics.ListAPIView):
+    renderer_classes = (r.CSVRenderer,) + tuple(api_settings.DEFAULT_RENDERER_CLASSES)
+
+    @extend_schema(
+        responses={200: [{}, ]},
+        parameters=[
+            OpenApiParameter(name='fields', description='comma separated list of fields', required=False, type=str),
+            OpenApiParameter(name='regions', description='comma separated list of regions', required=False, type=str),
+            OpenApiParameter(name='start_date', description='start_date filter, values are greater than specified',
+                             required=False, type=OpenApiTypes.DATE, default='2020-08-23'),
+            OpenApiParameter(name='end_date', description='end_date filter, values are below than specified',
+                             required=False, type=OpenApiTypes.DATE, default='2023-08-23')
+
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        data = list(self.get_queryset())
+        return Response(data)
+
+    def get_queryset(self):
+        params = self.request.query_params.dict()
+        params.update(self.kwargs)
+        params['regions'] = params['regions'].split(',') if 'regions' in params else []
+        params['fields'] = params['fields'].split(',') if 'fields' in params else []
+
+        serializer = DatasetRequestSerializer(data=params)
+        serializer.is_valid(raise_exception=True)
+
+        model = DatasetInfo.DATASETS[serializer.validated_data['dataset']]
+        queryset = model.objects
+
+        if model is RegionTransformedData and serializer.validated_data['regions']:
+            queryset = queryset.filter(region__in=serializer.validated_data['regions'])
+
+        start_date = serializer.validated_data.get('start_date')
+        if start_date:
+            queryset = queryset.filter(start_date__gt=start_date)
+
+        end_date = serializer.validated_data.get('end_date')
+        if end_date:
+            queryset = queryset.filter(end_date__lt=end_date)
+
+        return queryset.values(*serializer.validated_data['fields'])

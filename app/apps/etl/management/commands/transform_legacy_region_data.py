dfae2dbf593517a844b3e202e56fbb194682cfcd
Sergey Sergeev <sss30112001@yandex.ru>
2023-12-25 19:53:41 +0400

    add dags, update commands for dags

--- app/apps/etl/management/commands/transform_legacy_region_data.py
+++ app/apps/etl/management/commands/transform_legacy_region_data.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from apps.etl.utils.data_transformers.regional_transformers import LegacyRegionDataTransformer
 from apps.etl.utils.mappers.transformed_data_mappers import RegionTransformedDataMapper
@@ -7,6 +8,12 @@ from apps.etl.utils.mappers.transformed_data_mappers import RegionTransformedDat
 class Command(BaseCommand):
     help = "transforms and write legacy region data, data from external database"
 
+    def add_arguments(self, parser):
+        parser.add_argument("debug", nargs='?', type=str, default=settings.DEBUG)
+
     def handle(self, *args, **options):
         data = LegacyRegionDataTransformer.run()
-        RegionTransformedDataMapper().map(data)
+        if options['debug']:
+            RegionTransformedDataMapper().map(data)
+        else:
+            return data

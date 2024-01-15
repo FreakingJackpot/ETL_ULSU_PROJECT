9dba7306438811c5c8e554b587ee9615dd6726e1
Sergey Sergeev <sss30112001@yandex.ru>
2024-01-15 04:25:09 +0400

    add import_population Command

---
+++ app/apps/etl/management/commands/import_population.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+import requests
+from difflib import SequenceMatcher
+
+from django.core.management.base import BaseCommand
+
+from apps.etl.models import Population, Region
+from apps.etl.utils.parsers.rosstat_parser import RosstatParser
+from apps.etl.utils.data_transformers.population_transformer import PopulationTransformer
+from apps.etl.utils.mappers.population_mapper import PopulationMapper
+
+import time
+
+
+class Command(BaseCommand):
+    help = "parse, transform, insert/update population data from rosstat"
+
+    def add_arguments(self, parser):
+        parser.add_argument("all", type=int, help='0-latest information, 1-full available information', choices=(0, 1),
+                            default=0, nargs='?')
+
+    def handle(self, *args, **options):
+        data = RosstatParser(options['all']).get_parsed_data()
+        data = PopulationTransformer().run(data)
+        PopulationMapper().map(data)
